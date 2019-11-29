a7b269688d5e156da51faa0d002859e612872a8a
Shilpa Koliyottu <Shilpa.Koliyottu@neudesic.com>
2019-11-29 13:49:17 +0530

    Commencted code

--- Febinocci/Program.cs
+++ Febinocci/Program.cs
@@ -7,16 +7,16 @@ namespace Febinocci
     {
         static void Main(string[] args)
         {
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
+            //List<int> numList = new List<int>();
+            //numList.Add(0);
+            //numList.Add(1);
 
-            for (int i = 0; i < 10; i++)
-            {
-                numList.Add(numList[i] + numList[i + 1]);
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    numList.Add(numList[i] + numList[i + 1]);
 
-                Console.WriteLine(numList[i]);
-            }
+            //    Console.WriteLine(numList[i]);
+            //}
             Febinocci(10);
 
         }
